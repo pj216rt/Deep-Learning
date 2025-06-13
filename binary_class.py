422579e0a303fd008522d114a4016dc7bfbe0653
PALMER SWANSON <pjswanson@fsu.edu>
2025-06-12 23:08:08 -0400

    hotel booking example

---
+++ binary_class.py
@@ -0,0 +1,60 @@
+# Setup plotting
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-v0_8-whitegrid')
+# Set Matplotlib defaults
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+plt.rc('animation', html='html5')
+
+#loading in hotel cancellation dataset
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_transformer
+
+hotel = pd.read_csv('hotel_booking.csv')
+
+#copy the dataset
+X = hotel.copy()
+
+#remove the is cancelled column
+y = X.pop('is_canceled')
+
+#map months to numbers
+X['arrival_date_month'] = \
+    X['arrival_date_month'].map(
+        {'January':1, 'February': 2, 'March':3,
+         'April':4, 'May':5, 'June':6, 'July':7,
+         'August':8, 'September':9, 'October':10,
+         'November':11, 'December':12}
+    )
+
+#numerical features
+features_num = [
+    "lead_time", "arrival_date_week_number",
+    "arrival_date_day_of_month", "stays_in_weekend_nights",
+    "stays_in_week_nights", "adults", "children", "babies",
+    "is_repeated_guest", "previous_cancellations",
+    "previous_bookings_not_canceled", "required_car_parking_spaces",
+    "total_of_special_requests", "adr",
+]
+
+#categorical features
+features_cat = [
+    "hotel", "arrival_date_month", "meal",
+    "market_segment", "distribution_channel",
+    "reserved_room_type", "deposit_type", "customer_type",
+]
+
+transformer_num = make_pipeline(
+    SimpleImputer(strategy="constant"), # there are a few missing values
+    StandardScaler(),
+)
+transformer_cat = make_pipeline(
+    SimpleImputer(strategy="constant", fill_value="NA"),
+    OneHotEncoder(handle_unknown='ignore'),
+)
--- third.py
+++ third.py
@@ -24,3 +24,45 @@ X_train = df_train.drop('Class', axis=1)
 X_valid = df_valid.drop('Class', axis=1)
 y_train = df_train['Class']
 y_valid = df_valid['Class']
+
+#model portion
+from tensorflow import keras
+from tensorflow.keras import layers
+
+model = keras.Sequential([
+    layers.Dense(4, activation='relu', input_shape=[33]),
+    layers.Dense(4, activation='relu'),
+    layers.Dense(1, activation='sigmoid'),
+])
+
+#use binaryt accuracy and cross entropy
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['binary_accuracy'],
+)
+
+early_stopping = keras.callbacks.EarlyStopping(
+    patience=10,
+    min_delta=0.001,
+    restore_best_weights=True,
+)
+
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=512,
+    epochs=1000,
+    callbacks=[early_stopping],
+    verbose=0, # hide the output because we have so many epochs
+)
+
+history_df = pd.DataFrame(history.history)
+# Start the plot at epoch 5
+history_df.loc[5:, ['loss', 'val_loss']].plot()
+history_df.loc[5:, ['binary_accuracy', 'val_binary_accuracy']].plot()
+
+print(("Best Validation Loss: {:0.4f}" +\
+      "\nBest Validation Accuracy: {:0.4f}")\
+      .format(history_df['val_loss'].min(),
+              history_df['val_binary_accuracy'].max()))
