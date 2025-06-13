34760eb8963db91c4c7639719b9a8ee7896c79e9
PALMER SWANSON <pjswanson@fsu.edu>
2025-06-12 23:23:37 -0400

    hotel classification example

--- binary_class.py
+++ binary_class.py
@@ -50,11 +50,76 @@ features_cat = [
     "reserved_room_type", "deposit_type", "customer_type",
 ]
 
+#transforming the numerical values
 transformer_num = make_pipeline(
-    SimpleImputer(strategy="constant"), # there are a few missing values
+    SimpleImputer(strategy="constant"), # there are a few missing values.  These are filled with 0
     StandardScaler(),
 )
+
+#transform and fill missing values with NA
 transformer_cat = make_pipeline(
     SimpleImputer(strategy="constant", fill_value="NA"),
     OneHotEncoder(handle_unknown='ignore'),
 )
+
+preprocessor = make_column_transformer(
+    (transformer_num, features_num),
+    (transformer_cat, features_cat),
+)
+
+#split this tranformed data into train and validate sets, 75% train
+X_train, X_valid, y_train, y_valid = \
+    train_test_split(X, y, stratify=y, train_size=0.75)
+
+X_train = preprocessor.fit_transform(X_train)
+X_valid = preprocessor.transform(X_valid)
+
+input_shape = [X_train.shape[1]]
+
+#actually getting to the model now
+from tensorflow import keras
+from tensorflow.keras import layers
+
+model = keras.Sequential([
+    layers.BatchNormalization(input_shape=input_shape),
+    layers.Dense(256, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dropout(0.3),
+    layers.Dense(256, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dropout(0.3),
+    layers.Dense(1, activation='sigmoid'),
+])
+
+#adding cross-entropy as the loss function and the accuracy metric
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['binary_accuracy'],
+)
+
+#add an early stopping rule
+early_stopping = keras.callbacks.EarlyStopping(
+    patience=5,
+    min_delta=0.001,
+    restore_best_weights=True,
+)
+
+#fit the model now
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=512,
+    epochs=200,
+    callbacks=[early_stopping],
+)
+
+#plots
+history_df = pd.DataFrame(history.history)
+history_df.loc[:, ['loss', 'val_loss']].plot(title="Cross-entropy")
+plt.show()
+
+history_df.loc[:, ['binary_accuracy', 'val_binary_accuracy']].plot(title="Accuracy")
+plt.show()
+
+#accuracy rose
